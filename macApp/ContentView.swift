67f77c8f811b44fb22f475f679a2a82fd15c4767
Chris Rodriguez <v-rodrc@microsoft.com>
2020-06-04 14:32:20 -0500

    Initial Commit

---
+++ macApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  macApp
+//
+//  Created by Christopher Rodriguez on 6/4/20.
+//  Copyright © 2020 Christopher Rodriguez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
